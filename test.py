43a09a9a344ef5467be86ab47fca2099703965a2
Matthew Carbone <x94carbone@gmail.com>
2019-08-18 11:08:12 -0400

    Add initial files

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+__author__ = "Matthew Carbone"
+__maintainer__ = "Matthew Carbone"
+__email__ = "x94carbone@gmail.com"
+__status__ = "Prototype"
+
+
+if __name__ == '__main__':
+    print("hello world")
